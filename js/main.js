192796a20435b6c8d5265e6bc3697c26b0eb1e30
April Vanderslice <aprilage@icloud.com>
2018-11-04 12:20:59 -0600

    creates an array to grab and store participant names for easier access later

--- js/main.js
+++ js/main.js
@@ -7,14 +7,26 @@ document.addEventListener('DOMContentLoaded', () => {
   const submitParticipantNameElement = document.querySelector('[data-js="submit-participant"]');
   //3. Full list of participants
   const participantListElement = document.querySelector('[data-js="all-participants"]');
+  const participantMatcherButtonElement = document.querySelector('[]');
+
+
+  // Placeholder array to store all the participant's names in memory.
+  let listOfParticipants = [];
 
   //Grab new participants full name when "submit" it clicked
   //Submit the name to the full list of participants
   submitParticipantNameElement.addEventListener('click', (e) => {
     e.preventDefault();
     const currentParticipantNameValue = addNewParticipantNameElement.value;
+
+    // Along with building the HTML to be added to the page...
+    // I want to store this in an array, so that I can access it fast later.
+    listOfParticipants.push(currentParticipantNameValue);
+    console.log(listOfParticipants);
+
     const submittedParticipantName = `
-      <li data-js="participant"
+      <li
+        data-js="participant"
       >
         ${currentParticipantNameValue}
       </li>
