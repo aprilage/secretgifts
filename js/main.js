79ccdd0f620150c68af7c88609b142a395703d2a
April Vanderslice <aprilage@icloud.com>
2018-11-20 19:22:48 -0600

    commented JavaScript and CSS for better understanding and to make it easier for me to navigate when I start adding local storage

--- js/main.js
+++ js/main.js
@@ -4,13 +4,15 @@ function handlePartipantMatcherButtonClick(e, listOfParticipants) {
   let matches = [];
   const numberOfParticipants = listOfParticipants.length;
   if (numberOfParticipants % 2 === 0) {
-  // shuffle the list IF the number of participants is even
+  // if participant number is even, shuffle the list.
     const shuffledParticipants = _.shuffle(listOfParticipants);
     shuffledParticipants.map((participant, i) => {
       let indexToMatchWith = i + 1;
+      //matches the last person in the list with the first person
       if (indexToMatchWith >= listOfParticipants.length) {
         indexToMatchWith = 0;
       }
+      // once the list is shuffled, couple the names and create message that will notify user of mat
       const nextParticipant = shuffledParticipants[indexToMatchWith];
       const matchText = `${participant} buys for ${nextParticipant}`;
       matches.push(matchText);
@@ -20,9 +22,9 @@ function handlePartipantMatcherButtonClick(e, listOfParticipants) {
 
     matchedParticipantList.innerHTML = "";
       matches.forEach((match) => {
-        // adding matches to page
+        // creates HTML so matched message is visible to user
         const htmlToAdd = `
-          <li class="participantNames" id="matchedParticipants">
+          <li class="participantNamesMatched">
            ${match}
           </li>
         `;
@@ -39,13 +41,14 @@ function handlePartipantMatcherButtonClick(e, listOfParticipants) {
 
 //Wait for content to load
 document.addEventListener('DOMContentLoaded', () => {
-  //Element references
-  //1. Text input: participant's full name
+  // Element references
+  // 1. Text input: participant's full name
   const addNewParticipantNameElement = document.querySelector('[data-js="new-participant"]');
-  //2. Submit participant to the List
+  // 2. Submit participant to the List
   const submitParticipantNameElement = document.querySelector('[data-js="submit-participant"]');
-  //3. Full list of participants
+  // 3. Full list of participants
   const participantListElement = document.querySelector('[data-js="all-participants"]');
+  // 4. Button to make matches
   const participantMatcherButtonElement = document.querySelector('[data-js ="participant-matcher-button"]');
 
 
@@ -54,18 +57,19 @@ document.addEventListener('DOMContentLoaded', () => {
   // Placeholder array to store all the participant's names in memory.
   let listOfParticipants = [];
 
-  //Grab new participants full name when "submit" it clicked
+  //Grab new participants full name from text input when "submit" is clicked
   //Submit the name to the full list of participants
   submitParticipantNameElement.addEventListener('click', (e) => {
     e.preventDefault();
     const currentParticipantNameValue = addNewParticipantNameElement.value;
 
     // Along with building the HTML to be added to the page...
-    // I want to store this in an array, so that I can access it fast later.
+    // I want to store this in an array, so that I can access it quickly later.
     listOfParticipants.push(currentParticipantNameValue);
 
+//creates HTML to make the un-matched list of participants visible in the UI
     const submittedParticipantName = `
-      <li class="participantNames" id="allParticipants"
+      <li class="participantNames"
         data-js="participant"
       >
         ${currentParticipantNameValue}
